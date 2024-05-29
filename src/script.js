00ae291ae9aaf36736ddf753f1a88a0d71203910
main <larissachaves1425@gmail.com>
2024-05-29 17:15:09 -0400

    Aula final da PretaLab

--- src/script.js
+++ src/script.js
@@ -1,29 +1,29 @@
 let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
-let palpitejaDigitado =[];
-let palpiteDigitado = [];
+let tentativas = 0;
+let palpitesAnteriores = [];
 
-function adivinhacao() {
- 
-  let palpite = parseInt(prompt("Adivinhe o número (entre 1 e 100):"));
-
-  if (palpite < 1 || palpite > 100) {
-      alert("Por favor, insira um número entre 1 e 100.");
-      adivinhacao();
-  } else if (palpitejaDigitado.includes(palpiteDigitado)) {
-      alert("Número repetido. Tente novamente.");
-      adivinhacao();
-  } else {
-      palpitejaDigitado.push(palpiteDigitado);
-
-      if (palpiteDigitado === numeroAleatorio) {
-          alert(`Parabéns! Você acertou o número em ${palpite.length} palpites.`);
-      } else if (palpiteDigitado < numeroAleatorio) {
-          alert("Tente um número maior.");
-          adivinhacao();
-      } else {
-          alert("Tente um número menor.");
-          adivinhacao();
-      }
-  }
-}
+function pegarPalpiteDigitado() {
+    let palpite = parseInt(prompt("Digite um número entre 1 e 100:"));
     
+    if (palpite < 1 || palpite > 100) {
+        alert("Por favor, insira um número entre 1 e 100.");
+       
+    } else if (palpitesAnteriores.includes(palpite)) {
+        alert("Número repetido. Tente novamente.");
+     
+    } else {
+        tentativas++;
+        palpitesAnteriores.push(palpite);
+
+        if (palpite === numeroAleatorio) {
+            alert(`Parabéns! Você acertou o número em ${tentativas} tentativas.`);
+        } else if (palpite < numeroAleatorio) {
+            alert("Tente um número maior.");
+        
+        } else {
+            alert("Tente um número menor.");
+          
+    }
+}
+
+} 
