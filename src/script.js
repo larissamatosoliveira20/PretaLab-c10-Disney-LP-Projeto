eef4776d33bc31177ba6d486c359c9256b2c5ecd
main <larissachaves1425@gmail.com>
2024-05-31 22:42:53 -0400

    alterações novamente

--- src/script.js
+++ src/script.js
@@ -1,53 +1,56 @@
 let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
 let tentativas = 0;
-let palpitesAnteriores = [];
+let palpitesFeitos = [];
 
 function jogoDeAdivinhacao() {
-  const palpiteDigitado = palpiteDigitado();
+  const palpiteDigitado = pegarPalpiteDigitado();
 
-if (!palpiteDigitado) {
+  if (!palpiteDigitado || palpiteDigitado < 1 || palpiteDigitado > 100) {
     alert("Digite um número entre 1 e 100!");
+    return;
+  }
+    let repetido = false;
+  if (palpitesFeitos.includes(palpiteDigitado)) {
+    alert("Número repetido! Tente um número diferente.");
+    repetido = true;
+  } else {
+    palpitesFeitos.push(palpiteDigitado);
+  }
 
-}  if (palpite < 1 || palpite > 100) {
-  alert("Por favor, digitar entre 1 e 100.");
- } else if (palpitesAnteriores.includes(palpite)) {
-   alert("Número repetido. Tente novamente.")
-  
-   palpitesAnteriores.push(palpite);
-   if (palpiteDigitado === numeroAleatorio) {
-    alert("parabéns voce acertou o número") 
-   } else if (palpiteDigitado > numeroAleatorio) {
+  if (palpiteDigitado === numeroAleatorio) {
+    alert("Parabéns, você acertou o número!");
+    reiniciarJogo();
+  } else if (palpiteDigitado > numeroAleatorio) {
     tentativas++;
-    atualizarFeedback("O numero e muito alto,Tente novamente outro número.");
-   } else if (palpiteDigitado < numeroAleatorio) {
+    atualizarFeedback("O número é muito alto. Tente novamente outro número.");
+  } else if (palpiteDigitado < numeroAleatorio) {
     tentativas++;
-    atualizarFeedback("O número e muito baixo,Tente novamente outro número");
-   }
-    const novaPontuacao = 100 - (tentativas * 10);
-    atualizarPontuacao(novaPontuacao);
+    atualizarFeedback("O número é muito baixo. Tente novamente outro número.");
+  }
 
-    const palpitesFalhos = palpitesFalhos();
-    const novosPalpitesFalhos = palpitesFalhos + " " + palpiteDigitado;
-    atualizarPalpitesFalhos(novosPalpitesFalhos);
-    
-    const pontuacaoAtual = pegarPontuacao();
-    if (pontuacaoAtual === "Você tem 0 pontos") {
-        alert("Perdeu! Você chegou no limbo, acabou para você!");
-        reiniciarJogo();
-    }
-    function reiniciarJogo() {
-        const vaiReiniciar = confirm("Deseja reiniciar o jogo?");
-    
-        if (vaiReiniciar) {
-            numeroAleatorio = Math.floor(Math.random() * 100) + 1;
-            tentativas = 0;
-            palpitesAnteriores = [];
-            atualizarPalpitesFalhos("");
-            atualizarPontuacao(100);
-            atualizarFeedback("");
-            limparpalpiteDigitado();
-        }
-    }
+  const novaPontuacao = 100 - (tentativas * 10);
+  atualizarPontuacao(novaPontuacao);
 
+  const palpitesFalhos = pegarPalpitesFalhos();
+  const novosPalpitesFalhos = palpitesFalhos + " " + palpiteDigitado + (repetido ? "♻" : "");
+  atualizarPalpitesFalhos(novosPalpitesFalhos);
+
+  const pontuacaoAtual = pegarPontuacao();
+  if (pontuacaoAtual === "Você tem 0 pontos") {
+    alert("Perdeu! Você chegou no limbo, acabou para você!");
+    reiniciarJogo();
+  }
 }
-} 
+   function reiniciarJogo() {
+  const vaiReiniciar = confirm("Deseja reiniciar o jogo?");
+  
+  if (vaiReiniciar) {
+    numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+    tentativas = 0;
+    palpitesFeitos = [];
+    atualizarPalpitesFalhos("");
+    atualizarPontuacao(100);
+    atualizarFeedback("");
+    limparPalpiteDigitado();
+  }
+}
